a102c8c4aef0dada9ab9764e966359f182f8deba
mexists <mexists@yahoo.com>
2024-10-29 19:26:49 +0800

    sync/async append re-read

--- asyncAppend.js
+++ asyncAppend.js
@@ -4,14 +4,19 @@ function appendFile() {
 	console.log("Begin reading file");
 
 	fs.readFile("input.txt", "utf8", function (err, data) {
-		if (err) {
-			return console.error(err);
-		}
 		console.log(data);
 		console.log("End reading file");
 
 		console.log("Begin appending file");
-		
+		fs.appendFile("input.txt", "\nAdding new text here..", function (err) {
+			console.log("End appending file");
+
+			console.log("Begin re-reading file");
+			fs.readFile("input.txt", "utf8", function (err, newData) {
+				console.log(newData);
+				console.log("Finish re-reading file");
+			});
+		});
 	});
 }
 
