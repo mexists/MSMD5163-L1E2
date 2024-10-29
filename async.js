796c8086de590f5158c102f32f2d10aaf51b1fc3
mexists <mexists@yahoo.com>
2024-10-29 19:03:23 +0800

    fix sync/async

--- async.js
+++ async.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
 
 console.log("Begin reading");
-
 fs.readFile("input.txt", function (err, data) {
 	if (err) {
 		return console.error(err);
--- sync.js
+++ sync.js
@@ -1,8 +1,8 @@
 var fs = require("fs");
 
-console.log("Begin reading");
 
 var data = fs.readFileSync("input.txt");
 
+console.log("Begin reading");
 console.log(data.toString());
 console.log("End reading");
